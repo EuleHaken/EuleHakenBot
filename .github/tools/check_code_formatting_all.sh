a4152344057fa6e4c24cddd43e46048a792c9412
EuleHaken <sven220ce@gmail.com>
2022-06-30 19:03:20 +0200

    Used workflow from /chatterino/chatterino2 repo

--- .github/tools/check_code_formatting_all.sh
+++ .github/tools/check_code_formatting_all.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-find src/ \( -iname "*.hpp" -o -iname "*.cpp" \) -exec clang-format -style=file -i {} \;
+# Borrowed from: https://github.com/Chatterino/chatterino2/blob/master/tools/check-format.sh
+
+fail="0"
+
+while read -r file; do
+    if ! diff -u <(cat "$file") <(clang-format "$file"); then
+        echo "$file differs!"
+        fail="1"
+    fi
+done < <(find src/ -type f \( -iname "*.hpp" -o -iname "*.cpp" \))
+
+if [ "$fail" = "1" ]; then
+    echo "At least one file is poorly formatted - check the output above"
+    exit 1
+fi
+
+echo "Everything seems to be formatted properly! Good job"
