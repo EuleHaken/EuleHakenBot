4c58f0d53ebcadc543ea17fe35b199f9c1a922c1
EuleHaken <sven220ce@gmail.com>
2022-07-02 12:16:28 +0200

    made `main` function do nothing for now, basically

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,9 @@
-#include "MainWindow.hpp"
-#include "Response.hpp"
-
 #include <QApplication>
 #include <QDebug>
 
-using namespace EuleHakenBot;
-
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-
-    Response res = Response();
-    bool s = res.isFailure();
 
     return a.exec();
 }
