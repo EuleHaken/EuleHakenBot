52efab5d411cc35fb65ef89639c1cbd83f06d830
EuleHaken <sven220ce@gmail.com>
2022-07-15 14:38:34 +0200

    added struct `NetworkData`
    - holds the data about the request, such as the request type, callback when success, and so on...

---
+++ src/NetworkData.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "NetworkRequestType.hpp"
+#include "NetworkResult.hpp"
+
+#include <QNetworkRequest>
+#include <functional>
+
+namespace EuleHakenBot {
+
+struct NetworkData
+{
+    NetworkRequestType type;
+
+    QNetworkRequest request;
+
+    std::function<void(NetworkResult)> onSuccess;
+    std::function<void(NetworkResult)> onError;
+};
+
+}  // namespace EuleHakenBot
