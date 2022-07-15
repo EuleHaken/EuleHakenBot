a5092a971967685f40db7bc969165958142f832a
EuleHaken <sven220ce@gmail.com>
2022-07-15 14:58:05 +0200

    added NetworkRequest
    - takes care of creating the actual network request and then, by calling the `execute` method, executes the requests and sends it

---
+++ src/NetworkRequest.cpp
@@ -0,0 +1,100 @@
+#include "NetworkRequest.hpp"
+#include "NetworkManager.hpp"
+#include "PostToThread.hpp"
+
+namespace EuleHakenBot {
+
+NetworkRequest::NetworkRequest(const QString& url,
+                               const NetworkRequestType& type)
+    : _data{NetworkData()}
+{
+    this->_data.request.setUrl(QUrl{url});
+    this->_data.type = type;
+}
+
+NetworkRequest::NetworkRequest(const QUrl& url, const NetworkRequestType& type)
+    : _data{NetworkData()}
+{
+    this->_data.request.setUrl(url);
+    this->_data.type = type;
+}
+
+NetworkRequest NetworkRequest::onSuccess(std::function<void(NetworkResult)> cb)
+{
+    this->_data.onSuccess = cb;
+    return *this;
+}
+
+NetworkRequest NetworkRequest::onError(std::function<void(NetworkResult)> cb)
+{
+    this->_data.onError = cb;
+    return *this;
+}
+
+NetworkRequest NetworkRequest::setType(const NetworkRequestType& newType)
+{
+    this->_data.type = newType;
+    return *this;
+}
+
+void NetworkRequest::execute()
+{
+    this->_execute(std::move(this->_data));
+}
+
+void NetworkRequest::_execute(const NetworkData& data)
+{
+    auto reply = [data]() -> QNetworkReply* {
+        switch (data.type)
+        {
+            case NetworkRequestType::GET: {
+                return NetworkManager::accessManager.get(data.request);
+            }
+            case NetworkRequestType::POST: {
+                return nullptr; // TODO
+            }
+            default: {
+                return nullptr;
+            }
+        }
+    }();
+
+    if (reply == nullptr)
+    {
+        qInfo() << "Invalid request type!";
+        return;
+    }
+
+    auto handleReply = [data, reply]() mutable {
+        if (reply->error() != QNetworkReply::NoError)
+        {
+            qInfo() << "Error!"
+                    << "Status:"
+                    << reply->attribute(
+                           QNetworkRequest::Attribute::HttpStatusCodeAttribute);
+            return;
+        }
+
+        QByteArray byteData = reply->readAll();
+        auto status =
+            reply->attribute(QNetworkRequest::HttpStatusCodeAttribute);
+
+        NetworkResult result(byteData, status.toInt());
+
+        if (data.onSuccess)
+        {
+            data.onSuccess(result);
+        }
+
+        reply->deleteLater();
+    };
+
+    QObject::connect(reply, &QNetworkReply::finished, [data, handleReply]() {
+        postToThread([handleReply]() {
+            auto cb = std::move(handleReply);
+            cb();
+        });
+    });
+}
+
+}  // namespace EuleHakenBot
---
+++ src/NetworkRequest.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <QByteArray>
+#include <QNetworkReply>
+#include <QNetworkRequest>
+#include <QString>
+#include <QUrl>
+#include "NetworkData.hpp"
+#include "NetworkManager.hpp"
+#include "NetworkRequestType.hpp"
+
+namespace EuleHakenBot {
+
+class NetworkRequest
+{
+public:
+    NetworkRequest(const QString& url,
+                   const NetworkRequestType& type = NetworkRequestType::GET);
+
+    NetworkRequest(const QUrl& url,
+                   const NetworkRequestType& type = NetworkRequestType::GET);
+
+    NetworkRequest onSuccess(std::function<void(NetworkResult)> cb);
+    NetworkRequest onError(std::function<void(NetworkResult)> cb);
+
+    NetworkRequest setType(const NetworkRequestType& newType);
+
+    NetworkRequest setHeader(const char* key, const char* value);
+
+    void execute();
+
+private:
+    void _execute(const NetworkData& data);
+
+private:
+    NetworkData _data;
+};
+
+}  // namespace EuleHakenBot
