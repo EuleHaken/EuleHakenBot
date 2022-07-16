4697f59af6e80ca52a38662cd1cf09bb1dbf7bc8
EuleHaken <sven220ce@gmail.com>
2022-07-16 22:50:33 +0200

    added implementation for defined methods

--- src/Helix.cpp
+++ src/Helix.cpp
@@ -22,8 +22,33 @@ NetworkRequest Helix::makeRequest(const QString& url, const QUrlQuery& query)
 
 void Helix::getUsers()
 {
+    this->makeRequest("users").execute();
+}
+
+void Helix::getTwitchID(const QString& username,
+                        const std::function<void(int)>& onSuccess,
+                        const std::function<void(NetworkResult&)>& onError)
+{
     QUrlQuery query;
-    this->makeRequest("users", query).execute();
+    query.addQueryItem("login", username);
+
+    this->makeRequest("users", query)
+        .onSuccess([onSuccess](NetworkResult& result) {
+            auto id = result.jsonDocument()["data"]["id"];
+
+            onSuccess(id.toInt(-1));
+        })
+        .onError([onError](NetworkResult& result) {
+            onError(result);  // can prob. just do .onError(onError);
+        })
+        .execute();
+}
+
+void Helix::updateCredentials(const QString& newClientID,
+                              const QString& newOauthToken)
+{
+    this->clientID = newClientID;
+    this->oauthToken = newOauthToken;
 }
 
 }  // namespace EuleHakenBot
