a57fa34ce2062370d1412897643b7b22f5a2ca82
EuleHaken <sven220ce@gmail.com>
2022-07-16 22:48:37 +0200

    added method to get twitch ID for a specific user (by it's name/username)
    - also added a method to update client id and oauth token

--- src/Helix.hpp
+++ src/Helix.hpp
@@ -4,6 +4,8 @@
 
 #include <QString>
 #include <QUrlQuery>
+#include <functional>
+#include <typeinfo>
 
 namespace EuleHakenBot {
 
@@ -12,13 +14,30 @@ class Helix
 public:
     Helix();
 
-    NetworkRequest makeRequest(const QString& url, QUrlQuery& query);
-
     void getUsers();
     void getChannels();
 
+    void getTwitchID(const QString& username,
+                     const std::function<void(int)>& onSuccess,
+                     const std::function<void(NetworkResult&)>& onError =
+                         DefaultCallback<void, NetworkResult&>());
+
+    void updateCredentials(const QString& newClientID,
+                           const QString& newOauthToken);
+
 private:
-    QString baseUrl = "https://api.twitch.tv/helix/";
+    NetworkRequest makeRequest(const QString& url,
+                               const QUrlQuery& query = QUrlQuery());
+
+private:
+    const QString baseUrl = "https://api.twitch.tv/helix/%1";
+
+    QString clientID;
+    QString oauthToken;
+
+private:
+    template <typename Type, typename... Params>
+    using DefaultCallback = std::function<Type(Params...)>;
 };
 
 }  // namespace EuleHakenBot
