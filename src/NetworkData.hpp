7e36ed82eac11b5ad62ba5892f7597b5f7ad80ec
EuleHaken <sven220ce@gmail.com>
2022-07-19 21:02:52 +0200

    made default constructor private - added "type" (using ...) for onSuccess and onError

--- src/NetworkData.hpp
+++ src/NetworkData.hpp
@@ -3,13 +3,26 @@
 #include "NetworkRequestType.hpp"
 #include "NetworkResult.hpp"
 
+#include <QByteArray>
+#include <QCoreApplication>
+#include <QHttpMultiPart>
+#include <QNetworkReply>
 #include <QNetworkRequest>
 #include <functional>
 
 namespace EuleHakenBot {
 
+using SuccessCallback = std::function<void(NetworkResult&)>;
+using ErrorCallback = std::function<void(NetworkResult&)>;
+
 struct NetworkData
 {
+    Q_DISABLE_COPY(NetworkData) // meh, dont like that :/
+    NetworkData() = default;
+
+    SuccessCallback onSuccess;
+    ErrorCallback onError;
+
     NetworkRequestType type;
 
     QNetworkRequest request;
