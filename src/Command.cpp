bfeaf01073b1c7742e86f46cb5d814b00ec3e370
EuleHaken <sven220ce@gmail.com>
2022-07-25 18:02:54 +0200

    delete default constructor - added constructor with arguments

--- src/Command.cpp
+++ src/Command.cpp
@@ -1,3 +1,14 @@
 #include "Command.hpp"
 
-namespace EuleHakenBot {}
+namespace EuleHakenBot {
+
+Command::Command(const QString& name, const CodeFunc& code, int cooldown,
+                 const QString& description)
+    : _name{name}
+    , _code{code}
+    , _cooldown{cooldown}
+    , _description{description}
+{
+}
+
+}  // namespace EuleHakenBot
--- src/Command.hpp
+++ src/Command.hpp
@@ -1,9 +1,26 @@
 #pragma once
 
+#include <QString>
+#include <functional>
+
+namespace EuleHakenBot {
+
+using CodeFunc = std::function<QString()>;
 
 class Command
 {
-public:
-    Command();
+    Q_DISABLE_COPY(Command)
+
+protected:
+    Command() = delete;
+    Command(const QString& name, const CodeFunc& code, const int& cooldown,
+            const QString& description);
+
+protected:
+    QString _name;
+    CodeFunc _code;
+    int _cooldown;
+    QString _description;
 };
 
+}  // namespace EuleHakenBot
