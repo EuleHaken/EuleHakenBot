9d4c5afc9afb8df2a9a6975a391d9659209fe96e
EuleHaken <sven220ce@gmail.com>
2022-07-29 20:27:22 +0200

    added definitions and implementations to make class work

--- src/sql/SqlInsert.cpp
+++ src/sql/SqlInsert.cpp
@@ -1,6 +1,27 @@
 #include "SqlInsert.hpp"
 
-SqlInsert::SqlInsert()
-{
+#include "Paths.hpp"
 
+#include <QDir>
+
+namespace EuleHakenBot {
+
+SqlInsert::SqlInsert(const QString& table)
+    : _table{table}
+{
+    const QDir& root = Paths::getRootDir();
+
+    QString realDatabaseName = database;
+    if (!database.endsWith(".db"))
+        realDatabaseName.append(".db");
+
+    const QString& connectionName = root.absoluteFilePath(realDatabaseName);
+    if (!QSqlDatabase::contains(connectionName))
+    {
+        QSqlDatabase::addDatabase("QSQLITE", connectionName);
+    }
+
+    this->_db = QSqlDatabase::database(connectionName);
 }
+
+}  // namespace EuleHakenBot
--- src/sql/SqlInsert.hpp
+++ src/sql/SqlInsert.hpp
@@ -1,9 +1,33 @@
 #pragma once
 
+#include <QSqlDatabase>
+#include <QString>
 
-class SqlInsert
+namespace EuleHakenBot {
+
+class SqlInsert final
 {
 public:
-    SqlInsert();
+    /*!
+     * \param table Table to insert into
+     * \param database Database to insert into. Must not end with anything else than `.db`
+     *
+     * Creates a sql `INSERT` query
+     */
+    SqlInsert(const QString& table);
+
+public:
+    SqlInsert& data(const QString& key, const QString& value);
+
+    void exec();
+
+private:
+    QSqlDatabase _db;
+
+    QString _table;
+
+    QStringList _keys;
+    QStringList _vals;
 };
 
+}  // namespace EuleHakenBot
