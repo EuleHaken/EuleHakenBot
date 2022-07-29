f3df1bd349c312af431191bc0a95733e76c838c8
EuleHaken <sven220ce@gmail.com>
2022-07-29 21:00:47 +0200

    implement Tmi class

--- src/Tmi.cpp
+++ src/Tmi.cpp
@@ -1,6 +1,63 @@
 #include "Tmi.hpp"
 
-Tmi::Tmi()
-{
+namespace EuleHakenBot {
 
+void Tmi::getChatters(
+    const QString& channel,
+    const std::function<void(const bool& success, const QJsonObject&)>& cb)
+{
+    this->makeRequest(QString("group/user/%1/chatters").arg(channel))
+        .onSuccess([cb](const NetworkResult& result) {
+            if (result.status() != 200)
+            {
+                cb(false, QJsonObject());
+                return;
+            }
+
+            const auto& chattersObj =
+                result.jsonObject()["chatters"].toObject();
+            cb(true, chattersObj);
+        })
+        .onError([cb](const NetworkResult& result) {
+            cb(false, QJsonObject());
+        })
+        .execute();
 }
+
+void Tmi::getChatterCount(const QString& channel,
+                          const std::function<void(const int&)>& cb)
+{
+    this->makeRequest(QString("group/user/%1/chatters").arg(channel))
+        .onSuccess([cb](const NetworkResult& result) {
+            if (result.status() != 200)
+            {
+                cb(-1);
+                return;
+            }
+
+            const int& count = result.jsonObject()["chatter_count"].toInt(-1);
+            cb(count);
+        })
+        .onError([cb](NetworkResult&) {
+            cb(-1);
+        })
+        .execute();
+}
+
+Tmi& Tmi::getInstance()
+{
+    static Tmi instance;
+    return instance;
+}
+
+NetworkRequest Tmi::makeRequest(const QString& url, const QUrlQuery& query)
+{
+    assert(!url.startsWith("/"));
+
+    QUrl fullUrl(this->baseUrl.arg(url));
+    fullUrl.setQuery(query);
+
+    return NetworkRequest(fullUrl).setHeader("Accept", "application/json");
+}
+
+}  // namespace EuleHakenBot
--- src/Tmi.hpp
+++ src/Tmi.hpp
@@ -1,9 +1,55 @@
 #pragma once
 
+#include <QString>
+#include <QUrlQuery>
+
+#include "network/NetworkRequest.hpp"
+
+namespace EuleHakenBot {
 
 class Tmi
 {
+    Q_DISABLE_COPY(Tmi)
+
+private:
+    Tmi() = default;
+
 public:
-    Tmi();
+    /**
+     * @brief getChatters Gets all chatters that are currently in provided channel
+     * @param channel Channel to look for
+     * @param cb Contains the list of chatters - includes their role - and whether or not
+     *  the request was successful
+     *
+     * The roles are:
+     *  - broadcaster
+     *  - vips
+     *  - moderators
+     *  - staff
+     *  - admins
+     *  - global_mods
+     *  - viewers
+     */
+    void getChatters(
+        const QString& channel,
+        const std::function<void(const bool& success, const QJsonObject&)>& cb);
+
+    /**
+     * @brief getChatterCount Gets the amount of currently active chatters in provided channel
+     * @param channel Channel to look for
+     */
+    void getChatterCount(const QString& channel,
+                         const std::function<void(const int&)>& cb);
+
+public:
+    static Tmi& getInstance();
+
+private:
+    NetworkRequest makeRequest(const QString& url,
+                               const QUrlQuery& query = QUrlQuery());
+
+private:
+    const QString baseUrl = "https://tmi.twitch.tv/%1";
 };
 
+}  // namespace EuleHakenBot
