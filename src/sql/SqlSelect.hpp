0f804498163574bb22d31ab38762c199360f01c9
EuleHaken <sven220ce@gmail.com>
2022-07-30 00:08:50 +0200

    define `where`, `single`, `onSuccess`, `onError` and `exec` methods

--- src/sql/SqlSelect.hpp
+++ src/sql/SqlSelect.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
+#include <QMap>
 #include <QSqlDatabase>
 #include <QString>
 #include <QStringList>
+#include <QVariant>
 
 namespace EuleHakenBot {
 
@@ -15,12 +17,28 @@ public:
     SqlSelect& field(const QString& name);
     SqlSelect& fields(const QStringList& names);
 
+    SqlSelect& where(const QString& key, const QString& value);
+
+    SqlSelect& single();
+
+    SqlSelect& onSuccess(const SuccessCallback& cb);
+    SqlSelect& onError(const ErrorCallback& cb);
+
+    void exec();
+
 private:
     QSqlDatabase _db;
 
     QString _table;
 
     QStringList _fields;
+
+    QStringList _where;
+
+    bool _single{false};
+
+    SuccessCallback _onSuccess;
+    ErrorCallback _onError;
 };
 
 }  // namespace EuleHakenBot
