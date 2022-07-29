ec9d8ac9d4ce8bca4007d7874858d7c1a529033e
EuleHaken <sven220ce@gmail.com>
2022-07-30 00:08:07 +0200

    implement `onError` and `exec` method

--- src/sql/SqlSelect.cpp
+++ src/sql/SqlSelect.cpp
@@ -54,4 +54,81 @@ SqlSelect& SqlSelect::onSuccess(const SuccessCallback& cb)
     return *this;
 }
 
+SqlSelect& SqlSelect::onError(const ErrorCallback& cb)
+{
+    this->_onError = cb;
+
+    return *this;
+}
+
+void SqlSelect::exec()
+{
+    SqlWorker requester;
+    SqlWorker* worker = new SqlWorker;
+
+    auto onMakeQuery = [self = std::move(*this)]() mutable {
+        QString fields =
+            self._fields.length() == 0 ? "*" : self._fields.join(", ");
+
+        QString query =
+            QString("SELECT %1 FROM \"%2\"").arg(fields, self._table);
+
+        if (self._where.length() > 0)
+        {
+            QString where = self._where.join(") AND (");
+            query.append(QString(" WHERE (%1)").arg(where));
+        }
+
+        QSqlQuery result = self._db.exec(query);
+        if (result.lastError().type() != QSqlError::NoError)
+        {
+            if (self._onError)
+            {
+                self._onError(result);
+            }
+            else
+            {
+                qWarning() << "Insert query had an error:"
+                           << result.lastError();
+            }
+
+            return;
+        }
+        QList<QVariant> vals;
+
+        auto record = result.record();
+        int columns = record.count();
+        while (result.next())
+        {
+            for (int i = 0; i < columns; i++)
+            {
+                QMap<QString, QVariant> _map;
+                _map.insert(record.fieldName(i), record.field(i).value());
+
+                vals.insert(QVariant(map));
+            }
+            if (self._single)
+                break;
+        }
+
+        QVariant v;
+        v.toSt
+
+            if (self._onSuccess)
+        {
+            self._onSuccess(map);
+        }
+    };
+
+    QObject::connect(&requester, &SqlWorker::makeQuery, worker,
+                     [worker, &onMakeQuery, self = std::move(*this)]() mutable {
+                         onMakeQuery();
+
+                         self._db.close();
+                         delete worker;
+                     });
+
+    emit requester.makeQuery();
+}
+
 }  // namespace EuleHakenBot
