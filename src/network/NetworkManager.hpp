70a62cf8cce04167481b7b176df270ce3c51f1c8
EuleHaken <73948555+EuleHaken@users.noreply.github.com>
2022-07-31 11:48:35 +0200

    replaced comment with `README.md`

--- src/network/NetworkManager.hpp
+++ src/network/NetworkManager.hpp
@@ -5,8 +5,6 @@
 
 namespace EuleHakenBot {
 
-// Borrowed from https://github.com/Chatterino/chatterino2/blob/master/src/common/NetworkManager.hpp
-
 class NetworkManager : public QObject
 {
 public:
